fc575658cbfd8b7a79d6c586cf14ab85f6cb905a
IJayasimha <88708618+IJayasimha@users.noreply.github.com>
2021-08-24 06:29:31 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,36 @@
+<html>
+<head><title>Project</title>
+
+<body background="img2.png">
+<br/><br/><br/><br/><br/>
+<br/><br/><br/>
+<h1><center>E-certificates for Birth and Death<h1></center>
+ <br/>
+ <h3 style="color:green">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp; 
+&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+&nbsp; &nbsp; &nbsp;
+&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+
+&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+ &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<center>Officer-Login</center></h3>
+
+
+<form action="val.php" method="post">
+<center><table border=0>
+<tr><td><font size="4" color="brown"><b>Enter Name</b></font></td><td><input type="txt" name="name" </td></tr>
+<tr><td><font size="4" color="brown"><b>Enter Passwd</b></font></td><td><input type="password" name="password" </td></tr>
+</table></center><br/>
+<center>&nbsp; &nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
+<input type="submit" name="SUBMIT" value="SUBMIT" />
+&nbsp; <input type="reset" name="reset" value="CLEAR"/></center>
+<br/>
+</form>
+<p><h4 style="font-family:lucida calligraphy;color:blue"><center>"A New website 
+    to get E-certificates for Birth and Death".</center></h4></p>
+<center><h6 style="color:red"> *copy rights reserved.</h6></center>
+</body>
+<?php
+?>
+
+</head>
+</html>
